097012f9bd0a6e173730fd3383bda91b617d4695
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-04-25 01:17:24 +0700

    update tester

--- tester.py
+++ tester.py
@@ -1,7 +1,13 @@
 import cv2
 import TableLinesRemover as tlr
+import KKStructure as kk
 
+# structure = kk.KKStructure()
+# structure.execute("./output/kk_data.json")
 image_path = "./output/horizontal_part_3.png"
 img = cv2.imread(image_path)
 lines_remover = tlr.TableLinesRemover(img)
-lines_remover.execute()
+lines_remover.execute("./output/sliced_lower_table")
+
+
+
